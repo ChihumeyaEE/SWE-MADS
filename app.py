8ebfaa0a33c4ac40486dfa845d69bc3f8ecb173b
Abdullah Shehata <ashehata1308@gmail.com>
2022-04-05 15:00:37 -0400

    added requirements.txt and mini updates to models.py

--- app.py
+++ app.py
@@ -2,30 +2,32 @@ import os
 import flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
-from models import db,User
+from models import db, User, Cart
 
 load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
 # Point SQLAlchemy to your Heroku database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 # Gets rid of a warning
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # IMPORTANT: This must be AFTER creating db variable to prevent
 # circular import issues
 
 db.init_app(app)
 
+# creates all the classes(tables) in the models.py
 with app.app_context():
     db.create_all()
 
-@app.route('/')
+
+@app.route("/")
 def index():
-        return flask.render_template("index.html")
+    return flask.render_template("index.html")
 
 
 app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv("IP", "0.0.0.0"),
+    port=int(os.getenv("PORT", 8080)),
     debug=True,
 )
--- models.py
+++ models.py
@@ -2,7 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), nullable=False)
+
+
+class Cart(db.Model):
+    pass
