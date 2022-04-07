b3c147a9ac38d7f8f2eb52d819c6f90a118e6682
Chaudhary Danial <danialjattala@Danials-MBP.attlocal.net>
2022-04-07 12:26:05 -0400

    login and signup

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(100))
 
-
-class Cart(db.Model):
-    pass
+#class Cart(db.Model):
+   # __tablename__ = "cart"
+   # pass
