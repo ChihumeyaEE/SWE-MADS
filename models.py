99166f4f24bccf95808a70eeed4633159c9d11ad
ChihumeyaEE <meresiaeke@gmail.com>
2022-04-22 18:56:25 -0400

    formatted with black

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-# pylint: disable=invalid-name,no-member,unused-import,unused-variable,missing-module-docstring,missing-function-docstring,wrong-import-order,redefined-builtin,multiple-imports,invalid-envvar-default
+# pylint: disable=invalid-name,no-member,unused-import,unused-variable,missing-module-docstring,missing-function-docstring,wrong-import-order,redefined-builtin,multiple-imports,invalid-envvar-default,missing-class-docstring
 from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
@@ -23,10 +23,11 @@ class Post(db.Model):
     quantity = db.Column(db.Integer)
     description = db.Column(db.String(240), nullable=False)
 
+
 class Transactions(db.Model):
     __tablename__ = "transactions"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)   #current user
+    user_id = db.Column(db.Integer)  # current user
     post_id = db.Column(db.Integer)
     item_name = db.Column(db.String(120), nullable=False)
     quantity = db.Column(db.Integer)
